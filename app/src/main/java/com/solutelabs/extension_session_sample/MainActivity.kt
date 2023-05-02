1fabd0b28b7df12ae251c41dc2f4727238b4443a
Chirag Solanki <Chirag@launchpadapps.com.au>
2023-05-02 18:12:44 +0530

    Init Commit

---
+++ app/src/main/java/com/solutelabs/extension_session_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.solutelabs.extension_session_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
